4f277eb1281e91bf10fbd55965366a36bb452535
Jakub Kwiatkowski <jakub.m.kwiatkowski@gmail.com>
2022-03-23 14:51:06 +0100

    Test

--- ct_utils/radon.py
+++ ct_utils/radon.py
@@ -4,11 +4,11 @@ from .bresenham import bresenham
 
 
 def radon(img: ndarray, detector_count: int,
-          rotation_angles: ndarray[float]) -> ndarray:
+          rotation_angles: ndarray) -> ndarray:
 
     def cut_external(point):
         height, width = img.shape
-        x, y = point
+        x, y = [int(i) for i in point]
         return x >= 0 and x < width and y >= 0 and y < height
 
     max_size = max(*img.shape)
@@ -17,10 +17,12 @@ def radon(img: ndarray, detector_count: int,
 
     for row, angle in enumerate(rotation_angles):
         sinogram[row][0] = angle
-        _x = numpy.cos(angle) * max_size
-        _y = numpy.sin(angle) * max_size
+        _x = (numpy.cos(angle) * max_size)
+        _x = _x.astype(numpy.int64)
+        _y = (numpy.sin(angle) * max_size).astype('int')
+        print(type(_x), type(_y))
         for col in range(1, detector_count):
-            _offset = (col - 1) * space_between
+            _offset = ((col - 1) * space_between).astype('int')
             pixels = list(filter(cut_external, bresenham((-_x, -_y + _offset), (_x, _y + _offset))))
             sinogram[row][col] = sum([img[y][x] for x, y in pixels])
 
